c8b4ee98b4f378b84b73fac5bca80deee90edcf9
Shane <shaneosullivan4@gmail.com>
2014-09-06 22:36:04 +0100

    first commit

---
+++ android/src/com/shane/shuttlerunner/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.shane.shuttlerunner.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.shane.shuttlerunner.GameDriver;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameDriver(), config);
+	}
+}
---
+++ core/src/com/shane/shuttlerunner/update/GameUpdater.java
@@ -0,0 +1,16 @@
+package com.shane.shuttlerunner.update;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class GameUpdater {
+	
+	public void updateGame(){
+		Sprite batch = new Sprite();
+		Texture spaceShipImage = new Texture("spaceShip.png");
+		TextureRegion spaceShipImageRegion = new TextureRegion(spaceShipImage);
+		
+		
+	}
+}
