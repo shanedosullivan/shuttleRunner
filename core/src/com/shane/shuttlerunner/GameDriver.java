3bfa211a5454e419ab4e29a3ec994b803e918004
Shane <shaneosullivan4@gmail.com>
2014-09-06 23:36:23 +0100

    Uploaded images and made graphics helper

--- core/src/com/shane/shuttlerunner/GameDriver.java
+++ core/src/com/shane/shuttlerunner/GameDriver.java
@@ -5,14 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.shane.shuttlerunner.graphics.GameGraphicsHelper;
 
 public class GameDriver extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	GameGraphicsHelper graphicsHelper;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
+		
 		//img = new Texture("badlogic.jpg");
 	}
 
@@ -20,8 +24,10 @@ public class GameDriver extends ApplicationAdapter {
 	public void render () {
 		//Gdx.gl.glClearColor(1, 0, 0, 1);
 		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		//batch.begin();
+		graphicsHelper = new GameGraphicsHelper();
+		graphicsHelper.draw(batch);
+		//batch.draw(img, 0, 0);
+		//batch.end();
 	}
 }
